47eca1e58a2e513e1054b4b2cc916eb9939e0c2b
Ricardo Silva <7474175+romstk@users.noreply.github.com>
2025-06-05 23:03:20 -0300

    Remove conflict markers

--- src/App.js
+++ src/App.js
@@ -9,50 +9,7 @@ import Rodape from './componentes/Rodape/Rodape'
 
 function App() {
 
-<<<<<<< HEAD
-  const times = [
-    {
-      nome: 'Programação',
-      corPrimaria: '#57C278',
-      corSecundaria: '#D9F7E9' 
-    }, 
-    {
-      nome: 'Front-End',
-      corPrimaria: '#82CFFA',
-      corSecundaria: '#E8F8FF' 
-    },
-    {
-      nome: 'Data Science',
-      corPrimaria: '#A6D157',
-      corSecundaria: '#F0F8E2' 
-    },
-    {
-      nome: 'Devops',
-      corPrimaria: '#E06B69',
-      corSecundaria: '#FDE7E8' 
-    },
-    {
-      nome: 'UX e Design',
-      corPrimaria: '#DB6EBF',
-      corSecundaria: '#FAE9F5' 
-    },
-    {
-      nome: 'Mobile',
-      corPrimaria: '#FFBA05',
-      corSecundaria: '#FFF5D9' 
-    },
-    {
-      nome: 'Inovação e Gestão',
-      corPrimaria: '#FF8A29',
-      corSecundaria: '#FFEEDF' 
-    }, 
-    
-  ]
-
-  const [colaboradores, setColaboradores] = useState([])
-
-=======
-  const [times, setTimes] = useState([
+const [times, setTimes] = useState([
     {
       nome: 'Programação',
       cor: '#D9F7E9' 
@@ -230,10 +187,9 @@ function App() {
       time: times[5].nome
     },
   ]
-  const [colaboradores, setColaboradores] = useState(inicial)
->>>>>>> 2487590 (Curso Alura - React - Como os componentes funcionam. Aula 3. Como o react vê um componente. Criamos um input de cor para mudar a cor do Colaborador e do Time. Trabalhamos com o state para os Times, usamos as cores e o pacote Hex-to-RGBA para trabalhar com opacidade de cores e diferenciar a cor do time e colaborador.)
+const [colaboradores, setColaboradores] = useState(inicial)
 
-  const adicionarColaborador = (colaborador) => {
+const adicionarColaborador = (colaborador) => {
     
   /*debugger
 
@@ -248,20 +204,16 @@ function App() {
      setColaboradores([...colaboradores, colaborador])
 }
 
-<<<<<<< HEAD
-=======
 function mudarCorDoTime(cor, nomeTime) {
     setTimes(times.map(time => {
         if(time.nome === nomeTime){
             time.cor = cor;
-            
-            
         }
         return time;
     }));
 }
 
->>>>>>> 2487590 (Curso Alura - React - Como os componentes funcionam. Aula 3. Como o react vê um componente. Criamos um input de cor para mudar a cor do Colaborador e do Time. Trabalhamos com o state para os Times, usamos as cores e o pacote Hex-to-RGBA para trabalhar com opacidade de cores e diferenciar a cor do time e colaborador.)
+
 function deletarColaborador(colaborador) {
     console.log(`Deletar colaborador ${colaborador.nome}`)
 }
@@ -277,20 +229,13 @@ function deletarColaborador(colaborador) {
           // Ao executar esta função vai adicionar o colaborador na lista de colaboradores.
           //Como é feita a ligação? O App.js passa a função adicionarColaborador para o Formulario.js via props, atravevés de aoCadastrar. O Formulario dentro de suas funcionalidades cria o objeto com os dados do usuário e chama props.aoCadastrar(objeto). O App.js recebe o objeto e atualiza o estado. aoCadastar fica conectardo ao formulário pela propss aoCadastrar. A medidada que o formulário atualiza o estado do objeto colaborador lá no fomrulário a função aqui no App.js que recebe um colaborador como parâmetro recebe este colaborador e faz suas funcionalidade. isto é administrado peloa React através do conceito de estado e props.
           aoCadastrar={adicionarColaborador}  
-<<<<<<< HEAD
+
       />
 
-
-      <section className='time'>
-=======
-          
-      />
 
 
       <section className='times'>
         <h1>Minhar Organização</h1>
->>>>>>> 2487590 (Curso Alura - React - Como os componentes funcionam. Aula 3. Como o react vê um componente. Criamos um input de cor para mudar a cor do Colaborador e do Time. Trabalhamos com o state para os Times, usamos as cores e o pacote Hex-to-RGBA para trabalhar com opacidade de cores e diferenciar a cor do time e colaborador.)
-
           {times.map(time => <Time 
             key={time.nome} 
             //Aqui estou passando o time como props para o componente Time, que é um objeto com as propriedades nome, corPrimaria e corSecundaria
@@ -299,10 +244,8 @@ function deletarColaborador(colaborador) {
             colaboradoresTime={colaboradores.filter(colab => colab.time === time.nome)} 
             aoDeletar={deletarColaborador}
             //colaboradores={colaboradores.filter(colab => colab.time === time.nome)}
-<<<<<<< HEAD
-=======
+
             mudarCor={mudarCorDoTime}
->>>>>>> 2487590 (Curso Alura - React - Como os componentes funcionam. Aula 3. Como o react vê um componente. Criamos um input de cor para mudar a cor do Colaborador e do Time. Trabalhamos com o state para os Times, usamos as cores e o pacote Hex-to-RGBA para trabalhar com opacidade de cores e diferenciar a cor do time e colaborador.)
             
             />)}
 
