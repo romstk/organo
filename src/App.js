c43e0bc5432ed05cd2c334d0499c66b161f418b8
Ricardo Silva <7474175+romstk@users.noreply.github.com>
2025-06-05 22:09:21 -0300

    Aula 3 - Input de cor para Colaborador e Time

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Rodape from './componentes/Rodape/Rodape'
 
 function App() {
 
+<<<<<<< HEAD
   const times = [
     {
       nome: 'Programação',
@@ -50,6 +51,187 @@ function App() {
 
   const [colaboradores, setColaboradores] = useState([])
 
+=======
+  const [times, setTimes] = useState([
+    {
+      nome: 'Programação',
+      cor: '#D9F7E9' 
+    }, 
+    {
+      nome: 'Front-End',
+      cor: '#E8F8FF' 
+    },
+    {
+      nome: 'Data Science',
+      cor: '#F0F8E2' 
+    },
+    {
+      nome: 'Devops',
+      cor: '#FDE7E8' 
+    },
+    {
+      nome: 'UX e Design',
+      cor: '#FAE9F5' 
+    },
+    {
+      nome: 'Mobile',
+      cor: '#FFF5D9' 
+    },
+    {
+      nome: 'Inovação e Gestão',
+      cor: '#FFEEDF' 
+    }, 
+    
+  ]);
+
+  const inicial = [
+    {
+      nome: 'JULIANA AMOASEI',
+      cargo: 'Desenvolvedora de software e instrutora',
+      imagem: 'https://www.alura.com.br/assets/img/lideres/juliana-amoasei.1647533644.jpeg',
+      time: times[0].nome
+    },
+    {
+      nome: 'DANIEL ARTINE',
+      cargo: 'Engenheiro de Software na Stone Age',
+      imagem: 'https://www.alura.com.br/assets/img/lideres/daniel-artine.1647533644.jpeg',
+      time: times[0].nome
+    },
+    {
+      nome: 'GUILHERME LIMA',
+      cargo: 'Desenvolvedor Python e JavaScript na Alura',
+      imagem: '	https://www.alura.com.br/assets/img/lideres/guilherme-lima.1647533644.jpeg',
+      time: times[0].nome
+    },
+    {
+      nome: 'PAULO SILVEIRA',
+      cargo: 'Hipster e CEO da Alura',
+      imagem: 'https://www.alura.com.br/assets/img/lideres/paulo-silveira.1647533644.jpeg',
+      time: times[0].nome
+    },
+    {
+      nome: 'JULIANA AMOASEI',
+      cargo: 'Desenvolvedora de software e instrutora',
+      imagem: 'https://www.alura.com.br/assets/img/lideres/juliana-amoasei.1647533644.jpeg',
+      time: times[1].nome
+    },
+    {
+      nome: 'DANIEL ARTINE',
+      cargo: 'Engenheiro de Software na Stone Age',
+      imagem: 'https://www.alura.com.br/assets/img/lideres/daniel-artine.1647533644.jpeg',
+      time: times[1].nome
+    },
+    {
+      nome: 'GUILHERME LIMA',
+      cargo: 'Desenvolvedor Python e JavaScript na Alura',
+      imagem: '	https://www.alura.com.br/assets/img/lideres/guilherme-lima.1647533644.jpeg',
+      time: times[1].nome
+    },
+    {
+      nome: 'PAULO SILVEIRA',
+      cargo: 'Hipster e CEO da Alura',
+      imagem: 'https://www.alura.com.br/assets/img/lideres/paulo-silveira.1647533644.jpeg',
+      time: times[1].nome
+    },
+    {
+      nome: 'JULIANA AMOASEI',
+      cargo: 'Desenvolvedora de software e instrutora',
+      imagem: 'https://www.alura.com.br/assets/img/lideres/juliana-amoasei.1647533644.jpeg',
+      time: times[2].nome
+    },
+    {
+      nome: 'DANIEL ARTINE',
+      cargo: 'Engenheiro de Software na Stone Age',
+      imagem: 'https://www.alura.com.br/assets/img/lideres/daniel-artine.1647533644.jpeg',
+      time: times[2].nome
+    },
+    {
+      nome: 'GUILHERME LIMA',
+      cargo: 'Desenvolvedor Python e JavaScript na Alura',
+      imagem: '	https://www.alura.com.br/assets/img/lideres/guilherme-lima.1647533644.jpeg',
+      time: times[2].nome
+    },
+    {
+      nome: 'PAULO SILVEIRA',
+      cargo: 'Hipster e CEO da Alura',
+      imagem: 'https://www.alura.com.br/assets/img/lideres/paulo-silveira.1647533644.jpeg',
+      time: times[2].nome
+    },
+    {
+      nome: 'JULIANA AMOASEI',
+      cargo: 'Desenvolvedora de software e instrutora',
+      imagem: 'https://www.alura.com.br/assets/img/lideres/juliana-amoasei.1647533644.jpeg',
+      time: times[3].nome
+    },
+    {
+      nome: 'DANIEL ARTINE',
+      cargo: 'Engenheiro de Software na Stone Age',
+      imagem: 'https://www.alura.com.br/assets/img/lideres/daniel-artine.1647533644.jpeg',
+      time: times[3].nome
+    },
+    {
+      nome: 'GUILHERME LIMA',
+      cargo: 'Desenvolvedor Python e JavaScript na Alura',
+      imagem: '	https://www.alura.com.br/assets/img/lideres/guilherme-lima.1647533644.jpeg',
+      time: times[3].nome
+    },
+    {
+      nome: 'PAULO SILVEIRA',
+      cargo: 'Hipster e CEO da Alura',
+      imagem: 'https://www.alura.com.br/assets/img/lideres/paulo-silveira.1647533644.jpeg',
+      time: times[3].nome
+    },
+    {
+      nome: 'JULIANA AMOASEI',
+      cargo: 'Desenvolvedora de software e instrutora',
+      imagem: 'https://www.alura.com.br/assets/img/lideres/juliana-amoasei.1647533644.jpeg',
+      time: times[4].nome
+    },
+    {
+      nome: 'DANIEL ARTINE',
+      cargo: 'Engenheiro de Software na Stone Age',
+      imagem: 'https://www.alura.com.br/assets/img/lideres/daniel-artine.1647533644.jpeg',
+      time: times[4].nome
+    },
+    {
+      nome: 'GUILHERME LIMA',
+      cargo: 'Desenvolvedor Python e JavaScript na Alura',
+      imagem: '	https://www.alura.com.br/assets/img/lideres/guilherme-lima.1647533644.jpeg',
+      time: times[4].nome
+    },
+    {
+      nome: 'PAULO SILVEIRA',
+      cargo: 'Hipster e CEO da Alura',
+      imagem: 'https://www.alura.com.br/assets/img/lideres/paulo-silveira.1647533644.jpeg',
+      time: times[4].nome
+    },
+    {
+      nome: 'JULIANA AMOASEI',
+      cargo: 'Desenvolvedora de software e instrutora',
+      imagem: 'https://www.alura.com.br/assets/img/lideres/juliana-amoasei.1647533644.jpeg',
+      time: times[5].nome
+    },
+    {
+      nome: 'DANIEL ARTINE',
+      cargo: 'Engenheiro de Software na Stone Age',
+      imagem: 'https://www.alura.com.br/assets/img/lideres/daniel-artine.1647533644.jpeg',
+      time: times[5].nome
+    },
+    {
+      nome: 'GUILHERME LIMA',
+      cargo: 'Desenvolvedor Python e JavaScript na Alura',
+      imagem: '	https://www.alura.com.br/assets/img/lideres/guilherme-lima.1647533644.jpeg',
+      time: times[5].nome
+    },
+    {
+      nome: 'PAULO SILVEIRA',
+      cargo: 'Hipster e CEO da Alura',
+      imagem: 'https://www.alura.com.br/assets/img/lideres/paulo-silveira.1647533644.jpeg',
+      time: times[5].nome
+    },
+  ]
+  const [colaboradores, setColaboradores] = useState(inicial)
+>>>>>>> 2487590 (Curso Alura - React - Como os componentes funcionam. Aula 3. Como o react vê um componente. Criamos um input de cor para mudar a cor do Colaborador e do Time. Trabalhamos com o state para os Times, usamos as cores e o pacote Hex-to-RGBA para trabalhar com opacidade de cores e diferenciar a cor do time e colaborador.)
 
   const adicionarColaborador = (colaborador) => {
     
@@ -66,6 +248,20 @@ function App() {
      setColaboradores([...colaboradores, colaborador])
 }
 
+<<<<<<< HEAD
+=======
+function mudarCorDoTime(cor, nomeTime) {
+    setTimes(times.map(time => {
+        if(time.nome === nomeTime){
+            time.cor = cor;
+            
+            
+        }
+        return time;
+    }));
+}
+
+>>>>>>> 2487590 (Curso Alura - React - Como os componentes funcionam. Aula 3. Como o react vê um componente. Criamos um input de cor para mudar a cor do Colaborador e do Time. Trabalhamos com o state para os Times, usamos as cores e o pacote Hex-to-RGBA para trabalhar com opacidade de cores e diferenciar a cor do time e colaborador.)
 function deletarColaborador(colaborador) {
     console.log(`Deletar colaborador ${colaborador.nome}`)
 }
@@ -81,10 +277,19 @@ function deletarColaborador(colaborador) {
           // Ao executar esta função vai adicionar o colaborador na lista de colaboradores.
           //Como é feita a ligação? O App.js passa a função adicionarColaborador para o Formulario.js via props, atravevés de aoCadastrar. O Formulario dentro de suas funcionalidades cria o objeto com os dados do usuário e chama props.aoCadastrar(objeto). O App.js recebe o objeto e atualiza o estado. aoCadastar fica conectardo ao formulário pela propss aoCadastrar. A medidada que o formulário atualiza o estado do objeto colaborador lá no fomrulário a função aqui no App.js que recebe um colaborador como parâmetro recebe este colaborador e faz suas funcionalidade. isto é administrado peloa React através do conceito de estado e props.
           aoCadastrar={adicionarColaborador}  
+<<<<<<< HEAD
       />
 
 
       <section className='time'>
+=======
+          
+      />
+
+
+      <section className='times'>
+        <h1>Minhar Organização</h1>
+>>>>>>> 2487590 (Curso Alura - React - Como os componentes funcionam. Aula 3. Como o react vê um componente. Criamos um input de cor para mudar a cor do Colaborador e do Time. Trabalhamos com o state para os Times, usamos as cores e o pacote Hex-to-RGBA para trabalhar com opacidade de cores e diferenciar a cor do time e colaborador.)
 
           {times.map(time => <Time 
             key={time.nome} 
@@ -94,6 +299,10 @@ function deletarColaborador(colaborador) {
             colaboradoresTime={colaboradores.filter(colab => colab.time === time.nome)} 
             aoDeletar={deletarColaborador}
             //colaboradores={colaboradores.filter(colab => colab.time === time.nome)}
+<<<<<<< HEAD
+=======
+            mudarCor={mudarCorDoTime}
+>>>>>>> 2487590 (Curso Alura - React - Como os componentes funcionam. Aula 3. Como o react vê um componente. Criamos um input de cor para mudar a cor do Colaborador e do Time. Trabalhamos com o state para os Times, usamos as cores e o pacote Hex-to-RGBA para trabalhar com opacidade de cores e diferenciar a cor do time e colaborador.)
             
             />)}
 
--- src/componentes/Time/Time.js
+++ src/componentes/Time/Time.js
@@ -1,5 +1,6 @@
 import Colaborador from '../Colaborador/Colaborador'
 import './Time.css'
+<<<<<<< HEAD
 
 const Time = ({time, colaboradoresTime, aoDeletar}) => {
     return (
@@ -7,17 +8,36 @@ const Time = ({time, colaboradoresTime, aoDeletar}) => {
             //O Time é retornado e neste primeiro ponto o cabeçlho do card é criado com o nome do time e a cor primária.
             colaboradoresTime.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: time.corPrimaria }}>
             <h3 style={ {borderColor:time.corPrimaria} }>{time.nome}</h3>
+=======
+import hexToRgba from 'hex-to-rgba';
+
+const Time = ({time, colaboradoresTime, aoDeletar, mudarCor}) => {
+    return (
+            //O Time é retornado e neste primeiro ponto o cabeçlho do card é criado com o nome do time e a cor primária.
+            colaboradoresTime.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: hexToRgba(time.cor, '0.6') }}>
+            <input value={time.cor} type='color' className='input-cor' onChange={evento => mudarCor(evento.target.value, time.nome)} />
+            <h3 style={ {borderColor:time.cor} }>{time.nome}</h3>
+>>>>>>> 2487590 (Curso Alura - React - Como os componentes funcionam. Aula 3. Como o react vê um componente. Criamos um input de cor para mudar a cor do Colaborador e do Time. Trabalhamos com o state para os Times, usamos as cores e o pacote Hex-to-RGBA para trabalhar com opacidade de cores e diferenciar a cor do time e colaborador.)
             {/*Aqui mapeando os times crio cada colaborador propriamente dito retornando um map com cada um deles e suas propriedades*/}
             <div className='colaboradores'>
                 {colaboradoresTime.map((colaborador, indice) => 
                     {
                         return <Colaborador 
+<<<<<<< HEAD
                             corDeFundo = {colaborador.corPrimaria}
+=======
+                            corDeFundo = {time.cor}
+>>>>>>> 2487590 (Curso Alura - React - Como os componentes funcionam. Aula 3. Como o react vê um componente. Criamos um input de cor para mudar a cor do Colaborador e do Time. Trabalhamos com o state para os Times, usamos as cores e o pacote Hex-to-RGBA para trabalhar com opacidade de cores e diferenciar a cor do time e colaborador.)
                             key={indice} 
                             nome ={colaborador.nome} 
                             cargo={colaborador.cargo} 
                             imagem={colaborador.imagem}
+<<<<<<< HEAD
                             aoDeletar={aoDeletar}/>
+=======
+                            aoDeletar={aoDeletar}
+                            />
+>>>>>>> 2487590 (Curso Alura - React - Como os componentes funcionam. Aula 3. Como o react vê um componente. Criamos um input de cor para mudar a cor do Colaborador e do Time. Trabalhamos com o state para os Times, usamos as cores e o pacote Hex-to-RGBA para trabalhar com opacidade de cores e diferenciar a cor do time e colaborador.)
                     }
                 )}
             </div>
